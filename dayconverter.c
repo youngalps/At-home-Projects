46fea9d7ac653583d371cd2a27c1dd1831eada4b
youngalps <copenoah89@gmail.com>
2018-01-08 21:45:27 -0500

    days

---
+++ dayconverter.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+/*
+Write a C program that accepts 4 integers p, q, r, s from the user where r and s are positive and p is even. If q is greater than r and s is greater than p and if the sum of 
+Test Data :
+Input the second integer: 35 
+Input the third integer: 15
+Input the fourth integer: 46 
+Expected Output:
+Wrong values 
+
+
+
+R AND S NEED TO POSTIVE
+
+P IS EVEN
+
+if statement for Q
+*/
+
+int Calculate(int p, int q, int r, int s)
+{
+	
+}
+
+
+
+int main(void)
+{
+	int p;
+	int q;
+	int r;
+	int s;	
+
+
+	printf("Input a postive int:\n ");
+	scanf("%d",r)
+
+	printf("Input another postive int: \n");
+	scanf("%d",s)
+
+
+	printf("Input a even number: \n");
+	scanf("%d",p)
+
+
+	
+
+	
+	
+	
+}
